3b23ca9c4472a6632b137057e0ff98e0cb010833
Mansour Kheffache <mansourk@aucegypt.edu>
2017-02-15 16:59:40 +0200

    add prototype back-end skeleton

---
+++ app/utils/peer.js
@@ -0,0 +1,22 @@
+
+function getPeers() {
+  // get list of n best peers
+
+
+  return 0;
+}
+
+function sendShred() {
+  // send shred to specific host
+
+  return 0;
+}
+
+function receiveShred() {
+  // open channel/socket to receive a shred
+
+
+  return 0;
+}
+
+export { getPeers, sendShred, receiveShred};
