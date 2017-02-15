a821cd81cf1e1e368c2f4ca7290721cc8385836e
jimmy395454 <James94@aucegypt.edu>
2017-02-15 20:02:38 +0200

    add compression and decomression

--- app/utils/file.js
+++ app/utils/file.js
@@ -7,18 +7,39 @@ function getFileList() {
   return 0;
 }
 
-function compress(file) {
-  // compress file with zlib
-  const compressedFile = file + 0;
 
-  return compressedFile;
+
+  function compress(decompressedfilename,compressedfilename, callback) {
+  // compress file with zlib
+  console.log("started compression");
+  const zlib = require('zlib');
+  const gzip = zlib.createGzip();
+  const fs = require('fs');
+  const decompressedfile = fs.createReadStream(decompressedfilename);
+  const compressedfile = fs.createWriteStream(compressedfilename);
+  decompressedfile.pipe(gzip).pipe(compressedfile);
+  console.log("Done compressing");
+
+  if (callback) {
+         decompressedfile.on('end', callback);
+   }
+  return compressedfile;
 }
 
-function decompress(file) {
+  function decompress(compressedfilename,decompressedfilename, callback) {
   // decompress file with zlib
-  const decompressedFile = file + 0;
-
-  return decompressedFile;
+    console.log("started decompression");
+  const zlib = require('zlib');
+  const gunzip = zlib.createGunzip();
+  const fs = require('fs');
+  const compressedfile = fs.createReadStream(compressedfilename);
+  const decompressedfile = fs.createWriteStream(decompressedfilename);
+  compressedfile.pipe(gunzip).pipe(decompressedfile);
+  console.log("Done decompressing");
+  if (callback) {
+       compressedfile.on('end', callback);
+   }
+  return decompressedfile;
 }
 
 function encrypt(file, key) {
@@ -43,4 +64,4 @@ function updateFileMap(fileMapEntry) {
   return updatedFileMap;
 }
 
-export { createID, compress, decompress, shred, encrypt, decrypt, getFileList, updateFileMap };
+ module.exports =  { createID, compress, decompress, shred, encrypt, decrypt, getFileList, updateFileMap };
