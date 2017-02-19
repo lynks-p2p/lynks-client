95d865ba3c91b2d43eae0d4827bcf93663c96883
jimmy395454 <James94@aucegypt.edu>
2017-02-19 23:47:37 +0200

    changed the whole thing

--- app/utils/message.js
+++ app/utils/message.js
@@ -1,22 +1,40 @@
-//1. Can take operation ID and data as arguments respectively.
-//2. Can take no arguments at all
-//operation id lets the target node understand which operation it should execute
 
-module.exports = function Message() {
-  if(arguments.length==2){
-     this.operationID = arguments[0];
-     this.data = arguments[1];
-   }
-   else {
-     this.data="";
-     this.operationID="";
-   }
-    this.serialize = function() {
+class Message {
+  constructor () {
+    this.operationID="";
+    this.data="";
+  }
+    setValues(operationID,data){
+      this.operationID = operationID;
+      this.data = data;
+    }
+    serialize () {
         return JSON.stringify(this);
     }
-    this.deserialize = function(buf) {
+    deserialize (buf) {
         var msg = JSON.parse(buf);
         this.operationID = msg.operationID;
         this.data = msg.data;
     }
-};
+}
+class fileMessage extends Message {   //inherits from message
+    constructor()
+    {
+      super();
+      this.filename="";
+      this.filepath="";
+    }
+    setValues(operationID,data,filename,filepath){
+      super.setValues(operationID,data);
+      this.filename=filename;
+      this.filepath=filepath;
+    }
+    deserialize (buf) {
+        var msg = JSON.parse(buf);
+        this.operationID = msg.operationID;
+        this.data = msg.data;
+        this.filename = msg.filename;
+        this.filepath = msg.filepath;
+    }
+  }
+module.exports =  {Message,fileMessage};
