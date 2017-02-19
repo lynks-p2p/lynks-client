f7afb037ecc4eee6fd7a5121da61cba260b1b5dd
Mansour Kheffache <mansourk@aucegypt.edu>
2017-02-19 18:37:31 +0200

    add file receiving handler

--- app/utils/peer.js
+++ app/utils/peer.js
@@ -1,4 +1,9 @@
 
+import io from 'socket.io';
+import dl from 'delivery';
+import fs from 'fs';
+
+
 function getPeers() {
   // get list of n best peers
 
@@ -15,8 +20,23 @@ function sendShred() {
 function receiveShred() {
   // open channel/socket to receive a shred
 
+  // 5001 could be any random port else given that kadmelia gives the sender that random port
+  const listener = io.listen(5001);
+
+  listener.sockets.on('connection', (socket) => {
+    const delivery = dl.listen(socket);
+    delivery.on('receive.success', (file) => {
+      fs.writeFile(file.name, file.buffer, (err) => {
+        if (err) {
+          console.log('File could not be saved: ', err);
+        } else {
+          console.log('File ', file.name, ' saved');
+        }
+      });
+    });
+  });
 
   return 0;
 }
 
-export { getPeers, sendShred, receiveShred};
+export { getPeers, sendShred, receiveShred };
