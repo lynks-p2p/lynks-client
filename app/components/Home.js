e5e33923ad20386fb75c617061229eff353221e9
Chouaib Hamek <chouaib@aucegypt.edu>
2017-03-23 17:02:11 +0200

    Added app tab and file upload for Lynks Vault

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,15 +2,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
-
+import LynksVault from './LynksVault'
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tab: 1
+    };
+  }
   render() {
     return (
       <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+        <div className={styles.tab}>
+          <button className={styles.tablinks} onClick={() => { this.setState({ tab: 1 }); }}>
+            Lynks Vault
+          </button>
+          <button className={styles.tablinks} onClick={() => { this.setState({ tab: 2 }); }}>
+            Lynks Drive
+          </button>
+          <button className={styles.tablinks} onClick={() => { this.setState({ tab: 3 }); }}>
+            Lynks Share
+          </button>
+        </div>
+        <div className={styles.app}>
+          {this.state.tab === 1 ? <LynksVault /> : null }
         </div>
       </div>
     );
