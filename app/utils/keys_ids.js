79c13090ed8efe7244289b570dbc350db6738db9
Mansour Kheffache <mansourk@aucegypt.edu>
2017-04-29 13:59:03 +0200

    updated kets and ids generation

--- app/utils/keys_ids.js
+++ app/utils/keys_ids.js
@@ -6,53 +6,31 @@ import { readFileMap, createFileMap, addFileMapEntry } from './file';
 
 
 
-function generateFileID(callback) {
+function generateFileID(callback) {  // TODO: WHERE this will be called ?
 
-  // generate random file key (128 bits)
-  var key = crypto.randomBytes(16).toString('hex');
-
-  var fileMap = readFileMap();
-
-  // check if it's already in the filemap
-  while(fileMap[key]!=undefined)
-  {
-    var key = crypto.randomBytes(16).toString('hex');
-    console.log('1');
+    var key = crypto.randomBytes(16).toString('hex');  // generate random file key (128 bits)
+    readFileMap((fileMap) => {
+      while(fileMap[key] != undefined) { //  check if it's already in the filemap
+        var key = crypto.randomBytes(16).toString('hex');
+      }
+    });
+    return callback(key);
   }
 
-  console.log('2');
-  return callback(key);
-}
+function generateFileMapKey(userid, pin, callback) { // TODO: what is this ?
+    // hash userid with pin
+    var key = crypto.createHash('SHA256').update(userid).update(pin).digest('hex');
+    return callback(key);
+  }
 
-function generateFileMapKey(userid, pin, callback) {
-
-  // hash userid with pin
-  var key = crypto.createHash('SHA256').update(userid).update(pin).digest('hex');
-  return callback(key);
-}
-
-function generateMasterKey(FileMapKey, callback) {
-
+function generateMasterKey(FileMapKey, random, callback) {  // TODO: WHERE this will be called ?
   // salt the FileMapKey with a 32 bit string using hash
-  var key = crypto.createHash('SHA256').update(FileMapKey).update(crypto.randomBytes(4).toString('hex')).digest('hex');
+  var key = crypto.createHash('SHA256').update(FileMapKey).update(random).digest('hex');
   return callback(key);
 }
 
-function generateFileKey(MasterKey, FileID, callback) {
-
+function generateFileKey(MasterKey, FileID, callback) { // TODO: WHERE this will be called ?
   // hash MasterKey with FileID
   var key = crypto.createHash('SHA256').update(MasterKey).update(FileID).digest('hex');
   return callback(key);
 }
-
-// generateFileID(    (mykey)=>{
-//   console.log(mykey);
-// });
-
-// generateFileMapKey( 'ahmad_adel', '12345', (mykey)=>{
-//   console.log(mykey);
-// });
-
-generateFileKey( 'ahmad_adel', '135yw534ws6y34', (mykey)=>{
-  console.log(mykey);
-});
