be0cddf21bb13b2d010995ad888214c13e2ba199
Ahmad Adel <ahmadabdalla@aucegypt.edu>
2017-05-04 09:06:41 +0200

    added shredsize as a param to getPeers function, created an initSubscribe function, moved test files to draft_tests,removed unnecessary function params

--- app/utils/peer.js
+++ app/utils/peer.js
@@ -144,41 +144,7 @@ function initHost( port, networkID, seed, callback) {
 
       console.log('Tracking using the Defaults activityPath');
 
-      node.plugin(quasar);
-
-      node.quasarSubscribe('icanhost', (broadcast) => {
-          console.log('recieved sth');
-          loadActivityPattern((myactivity)=>{
-
-              var mycontent = {
-                  ip: ip.address(),
-                  port: port,
-                  id: node.identity.toString('hex'),
-                  activity: myactivity
-              }
-
-              var uploaderip = broadcast['ip'];
-              var uploaderport = broadcast['port'];
-              var shredsize = broadcast['shred_size'];
-
-              if(remaining_capacity > shredsize)
-              {
-                  const socket = socketclient(`http://${uploaderip}:${uploaderport}`);
-
-                  socket.on('connect', function (socket) {
-                      console.log('Connected!');
-                  });
-
-                  socket.on('send.success', (yo) => {
-                      console.log(yo);
-                      socket.disconnect();
-                  });
-
-                  socket.emit('subscriber', mycontent);
-              }
-          });
-      });
-      console.log('Subscribed');
+      initSubscribe();      // subscribe to topic for hosting shreds
 
       trackActivityPattern(); // no callback (running function as long as the app is used )
 
@@ -189,8 +155,49 @@ function initHost( port, networkID, seed, callback) {
 
 
 
+function initSubscribe()
+{
+    node.plugin(quasar);
 
-function getPeers(callback, newhosts)
+    node.quasarSubscribe('icanhost', (broadcast) => {
+        console.log('recieved a broadcast');
+        loadActivityPattern((myactivity)=>{
+
+            var mycontent = {
+                ip: ip.address(),
+                port: port,
+                id: node.identity.toString('hex'),
+                activity: myactivity
+            }
+
+            var uploaderip = broadcast['ip'];
+            var uploaderport = broadcast['port'];
+            var shredsize = broadcast['shred_size'];
+
+            if(remaining_capacity > shredsize)
+            {
+                const socket = socketclient(`http://${uploaderip}:${uploaderport}`);
+
+                socket.on('connect', function (socket) {
+                    console.log('Connected!');
+                });
+
+                socket.on('send.success', (yo) => {
+                    console.log(yo);
+                    socket.disconnect();
+                });
+
+                socket.emit('subscriber', mycontent);
+            }
+        });
+    });
+    console.log('Subscribed to icanhost.');
+}
+
+
+
+
+function getPeers(shredsize, callback)
 {
     var counter = 0;
 
@@ -201,7 +208,7 @@ function getPeers(callback, newhosts)
     var broadcast = {
         ip: ip.address(),
         port: 3000,
-        shred_size: 90
+        shred_size: shredsize
     }
 
     node.quasarPublish('icanhost', broadcast);
@@ -215,7 +222,7 @@ function getPeers(callback, newhosts)
             counter++;
 
             console.log(counter);
-            if(counter==3)
+            if(counter>=90)
             {
                 console.log('should disconnect now');
                 http.close();
@@ -310,7 +317,7 @@ function trackActivityPattern( deltaMinutes, activityDays, activityPath ) {  /* 
 }
 
 
-function calculateMatching(hostactivity, callback, score) {            // the function recieves the host's activity in array form
+function calculateMatching(hostactivity, callback) {            // the function recieves the host's activity in array form
 
     const deltaMinutes =  10; // update activity every 10 min
     const activityDays =  7; // activity for 1 week
@@ -342,7 +349,7 @@ function calculateMatching(hostactivity, callback, score) {            // the fu
 }
 
 
-function calculateHostAvailability(hostactivity, callback, score) {            // the function recieves the host's activity in array form
+function calculateHostAvailability(hostactivity, callback) {            // the function recieves the host's activity in array form
 
     const deltaMinutes =  10; // update activity every 10 min
     const activityDays =  7; // activity for 1 week
